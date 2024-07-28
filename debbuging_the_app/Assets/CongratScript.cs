9c826512ae4cd4e1bcbad7d44cf6a4b345541b6c
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-27 22:07:53 -0300

    1.4 commit

--- debbuging_the_app/Assets/CongratScript.cs
+++ debbuging_the_app/Assets/CongratScript.cs
@@ -1,50 +1,53 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CongratScript : MonoBehaviour
 {
-    public TextMesh Text;
-    public ParticleSystem SparksParticles;
+    public TextMesh text;
+    public ParticleSystem sparksParticles;
     
-    private List<string> TextToDisplay;
+    private List<string> textToDisplay;
     
-    private float RotatingSpeed;
-    private float TimeToNextText;
+    private float rotatingSpeed;
+    private float timeToNextText;
 
-    private int CurrentText;
+    private int currentText;
     
     // Start is called before the first frame update
     void Start()
     {
-        TimeToNextText = 0.0f;
-        CurrentText = 0
+        timeToNextText = 0.0F;
+        currentText = 0;
         
-        RotatingSpeed = 1.0;
+        rotatingSpeed = 1.0F;
 
-        TextToDisplay.Add("Congratulation");
-        TextToDisplay.Add("All Errors Fixed");
+        textToDisplay = new List<string>();
 
-        Text.text = TextToDisplay[0];
+        textToDisplay.Add("Congratulation");
+        textToDisplay.Add("All Errors Fixed");
+
+        text.text = textToDisplay[0];
         
-        SparksParticles.Play();
+        sparksParticles.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        TimeToNextText += Time.deltaTime;
+        timeToNextText += Time.deltaTime;
 
-        if (TimeToNextText > 1.5f)
+        if (timeToNextText > 1.5F)
         {
-            TimeToNextText = 0.0f;
+            timeToNextText = 0.0F;
             
-            CurrentText++;
-            if (CurrentText >= TextToDisplay.Count)
+            currentText++;
+            if (currentText >= textToDisplay.Count)
             {
-                CurrentText = 0;
+                currentText = 0;
+            }
 
-
-            Text.text = TextToDisplay[CurrentText];
+            text.text = textToDisplay[currentText];
         }
     }
 }
